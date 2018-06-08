7efa14f2d425e0cbe19f801c6cc6a0f58bbac66e
Alexandre Moureaux <Almouro@users.noreply.github.com>
2018-06-08 16:08:51 +0200

    Create new Sela child theme (#1)
    * Remove stuff from sela theme
    
    * Create new Sela child theme

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,3 +1,3 @@
-ssh amoureaux@gbu.fr 'mkdir -p sela'
-scp -r [!.]* amoureaux@gbu.fr:~/sela
-ssh amoureaux@gbu.fr 'cd /home/gbu-clone/gbl/wp-content/themes && rm -rf sela && mv ~/sela .'
+ssh amoureaux@gbu.fr 'mkdir -p sela-child'
+scp -r [!.]* amoureaux@gbu.fr:~/sela-child
+ssh amoureaux@gbu.fr 'cd /home/gbu-clone/gbl/wp-content/themes && rm -rf sela-child && mv ~/sela-child .'
